7be57c465d8cbeb93ebb097b050a74962290465a
bgreiner <bgreiner2@huskers.unl.edu>
2024-11-15 13:50:42 -0600

    Added balance column to Balance_Table Changed variable types of some table columns from smallint --> int for consistency Added Try-Catch statements to table creations Added resultTime to Result_Table

--- database/CardGameDB.sql
+++ database/CardGameDB.sql
@@ -1,71 +1,121 @@
 --Database Table Creation
 
 --Check for pre-existing table, build if none exist
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'User_Table')
+BEGIN TRY
 	BEGIN
-	PRINT 'User Table Loaded'
-	END
-ELSE
-	BEGIN
-    PRINT 'User Table Building'
 	CREATE TABLE User_Table(
 		userID int primary key not null,
 		username varchar(64) not null unique,
 		passHash varchar(64) not null unique,
 		email varchar(128) unique,
 		creationDate datetime default getdate() not null,
-		lastLogin datetime default getdate() not null);
+		lastLogin datetime default getdate() not null
+	);
 	END
+	PRINT 'User Table Building'
+END TRY
+BEGIN CATCH
+	IF ERROR_NUMBER() = 2714
+		BEGIN
+		PRINT 'User Table Loaded'
+		END
+	ELSE
+		SELECT
+		ERROR_NUMBER() AS ErrorNumber,
+		ERROR_STATE() AS ErrorState,
+		ERROR_SEVERITY() AS ErrorSeverity,
+		ERROR_PROCEDURE() AS ErrorProcedure,
+		ERROR_LINE() AS ErrorLine,
+		ERROR_MESSAGE() AS ErrorMessage;
+END CATCH;
+
 
 
 --Check for pre-existing table, build if none exist
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Balance_Table')
+BEGIN TRY
 	BEGIN
-	PRINT 'Balance Table Loaded'
-	END
-ELSE
-    BEGIN
-	PRINT 'Balance Table Building'
 	CREATE TABLE Balance_Table(
-		ID int not null primary key,
-
+		balanceID int not null primary key,
 		--should delete all records from a given user if userID is deleted
 		userID int not null foreign key references User_Table(userID) on delete cascade, 
-		reason varchar(128));
-
+		currentBalance int not null,
+		reason varchar(128)
+	);
 	--should make retrieving data by userID faster
 	CREATE INDEX BalanceIndexByUserID ON Balance_Table(userID);
+	PRINT 'Balance Table Building'
+
 	END
+END TRY
+BEGIN CATCH
+	IF ERROR_NUMBER() = 2714
+		BEGIN
+			PRINT 'Balance Table Loaded'
+		END
+	ELSE
+		SELECT
+		ERROR_NUMBER() AS ErrorNumber,
+		ERROR_STATE() AS ErrorState,
+		ERROR_SEVERITY() AS ErrorSeverity,
+		ERROR_PROCEDURE() AS ErrorProcedure,
+		ERROR_LINE() AS ErrorLine,
+		ERROR_MESSAGE() AS ErrorMessage;
+END CATCH;
 
 
 --Check for pre-existing table, build if none exist
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Result_Table')
+BEGIN TRY
 	BEGIN
-	PRINT 'Result Table Loaded'
-	END
-ELSE
-	BEGIN
-	PRINT 'Result Table Building'
 	CREATE TABLE Result_Table(
-		gameID smallint primary key not null,
-		outcome smallint not null, --0 for loss, 1 for win, -1 for tie
-		userID int not null foreign key references User_Table(userID));
+		gameID int primary key not null,
+		outcome int not null, --0 for loss, 1 for win, -1 for tie
+		userID int not null foreign key references User_Table(userID),
+		resultTime datetime default getdate() not null
+	);
+	PRINT 'Result Table Building'
 	END
+END TRY
+BEGIN CATCH
+	IF ERROR_NUMBER() = 2714
+		BEGIN
+			PRINT 'Result Table Loaded'
+		END
+	ELSE
+		SELECT
+		ERROR_NUMBER() AS ErrorNumber,
+		ERROR_STATE() AS ErrorState,
+		ERROR_SEVERITY() AS ErrorSeverity,
+		ERROR_PROCEDURE() AS ErrorProcedure,
+		ERROR_LINE() AS ErrorLine,
+		ERROR_MESSAGE() AS ErrorMessage;
+END CATCH;
+
 
 
 	--Check for pre-existing table, build if none exist
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Bet_Table')
+BEGIN TRY
 	BEGIN
-	PRINT 'Bet Table Loaded'
-	END
-ELSE
-	BEGIN
-	PRINT 'Bet Table Building'
 	CREATE TABLE Bet_Table(
-		gameID smallint not null foreign key references Result_table(gameID),
 		userID int not null foreign key references User_Table(userID),
+		gameID int not null foreign key references Result_Table(gameID),
 		betAmount int not null,
-		betOutcome smallint not null
+		betOutcome int not null
 	);
 	CREATE INDEX betIndexByUserID ON Bet_Table(userID);
+	PRINT 'Bet Table Building'
 	END
+END TRY
+BEGIN CATCH
+	IF ERROR_NUMBER() = 2714
+		BEGIN
+			PRINT 'Bet Table Loaded'
+		END
+	ELSE
+		SELECT
+		ERROR_NUMBER() AS ErrorNumber,
+		ERROR_STATE() AS ErrorState,
+		ERROR_SEVERITY() AS ErrorSeverity,
+		ERROR_PROCEDURE() AS ErrorProcedure,
+		ERROR_LINE() AS ErrorLine,
+		ERROR_MESSAGE() AS ErrorMessage;
+END CATCH;
