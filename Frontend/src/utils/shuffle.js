056fed04884a693a52c0497c84c5e3daab0f4860
Charlie Mciver <cmciver2@huskers.unl.edu>
2024-12-04 21:10:54 -0600

    added shuffling component

---
+++ Frontend/src/utils/shuffle.js
@@ -0,0 +1,8 @@
+﻿// utils/shuffle.js
+export const shuffleDeck = (deck) => {
+    for (let i = deck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+    return deck;
+};
