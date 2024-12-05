6970bf4057bb94ab0a0745793ccf83bf84d1f012
Charlie Mciver <cmciver2@huskers.unl.edu>
2024-12-04 21:10:34 -0600

    added deck component

---
+++ Frontend/src/utils/deck.js
@@ -0,0 +1,14 @@
+﻿// utils/deck.js
+export const createDeck = () => {
+    const suits = ['♠', '♣', '♥', '♦'];
+    const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    const deck = [];
+
+    for (const suit of suits) {
+        for (const value of values) {
+            deck.push({ suit, value });
+        }
+    }
+
+    return deck;
+};
